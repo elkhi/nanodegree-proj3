830ed730cf4af676e6cc7e1c57cc843811caa245
kai <klim.mackenzie@live.com>
2018-01-14 20:54:01 +0000

    Complete

--- project-3.py
+++ project-3.py
@@ -7,12 +7,14 @@
 easy_words = ['programming', 'language', 'whitespace', 'readability']
 
 easy_string = """
-Python is a widely used high-level __word_1__ __word_2__ for general-purpose __word_1__,
-created by Guido van Rossum and first released in 1991. An interpreted __word_2__, Python has a 
-design philosophy that emphasizes code __word_4__ (notably using __word_3__ indentation to delimit 
-code blocks rather than curly brackets or keywords), and a syntax that allows programmers to express 
-concepts in fewer lines of code than might be used in __word_1__s such as C++ or Java. The __word_2__ provides 
-constructs intended to enable writing clear programs on both a small and large scale."""
+Python is a widely used high-level __word_1__ __word_2__ for general-purpose 
+__word_1__, created by Guido van Rossum and first released in 1991. An 
+interpreted __word_2__, Python has a design philosophy that emphasizes code 
+__word_4__ (notably using __word_3__ indentation to delimit code blocks rather 
+than curly brackets or keywords), and a syntax that allows programmers to 
+express concepts in fewer lines of code than might be used in __word_1__s such 
+as C++ or Java. The __word_2__ provides constructs intended to enable writing 
+clear programs on both a small and large scale."""
 
 
 #For the medium quiz, the text is from the History box:
@@ -20,19 +22,25 @@ constructs intended to enable writing clear programs on both a small and large s
 medium_words = ['paradigm', 'garbage', 'Beautiful', 'implicit', 'Complicated']
 
 medium_string = '''
-Python is a multi-__word_1__ programming language: object-oriented programming and structured programming are fully 
-supported, and many language features support functional programming and aspect-oriented programming (including by
-metaprogramming and metaobjects (magic methods)). Many other __word_1__s are supported via extensions, including design 
-by contract and logic programming.
+Python is a multi-__word_1__ programming language: object-oriented programming 
+and structured programming are fully supported, and many language features 
+support functional programming and aspect-oriented programming (including by
+metaprogramming and metaobjects (magic methods)). Many other __word_1__s are 
+supported via extensions, including design by contract and logic programming.
 
-Python uses dynamic typing and a mix of reference counting and a cycle-detecting __word_2__ collector for memory management. 
-An important feature of Python is dynamic name resolution (late binding), which binds method and variable names during program execution.
+Python uses dynamic typing and a mix of reference counting and a 
+cycle-detecting __word_2__ collector for memory management. An important 
+feature of Python is dynamic name resolution (late binding), which binds 
+method and variable names during program execution.
 
-The design of Python offers some support for functional programming in the Lisp tradition. The language has filter(), map(), 
-and reduce() functions; list comprehensions, dictionaries, and sets; and generator expressions. The standard library has 
-two modules (itertools and functools) that implement functional tools borrowed from Haskell and Standard ML.
+The design of Python offers some support for functional programming in the 
+Lisp tradition. The language has filter(), map(), and reduce() functions; list 
+comprehensions, dictionaries, and sets; and generator expressions. The 
+standard library has two modules (itertools and functools) that implement 
+functional tools borrowed from Haskell and Standard ML.
 
-The core philosophy of the language is summarized by the document The Zen of Python (PEP 20), which includes aphorisms such as:
+The core philosophy of the language is summarized by the document The Zen of 
+Python (PEP 20), which includes aphorisms such as:
 
     __word_3__ is better than ugly
     Explicit is better than __word_4__
@@ -47,32 +55,44 @@ The core philosophy of the language is summarized by the document The Zen of Pyt
 hard_words = ['functionality', 'core', 'optimization', 'Cython', 'Monty Python', 'pythonic', 'unpythonic']
 
 hard_string = '''
-Rather than requiring all desired __word_1__ to be built into the language's __word_2__, Python was designed to be highly extensible. 
-Python can also be embedded in existing applications that need a programmable interface. This design of a small core language 
-with a large standard library and an easily extensible interpreter was intended by Van Rossum from the start because of his 
-frustrations with ABC, which espoused the opposite mindset.
+Rather than requiring all desired __word_1__ to be built into the language's 
+__word_2__, Python was designed to be highly extensible. Python can also be 
+embedded in existing applications that need a programmable interface. This 
+design of a small core language with a large standard library and an easily 
+extensible interpreter was intended by Van Rossum from the start because of 
+his frustrations with ABC, which espoused the opposite mindset.
 
-While offering choice in coding methodology, the Python philosophy rejects exuberant syntax, such as in Perl, in favor of a 
-sparser, less-cluttered grammar. As Alex Martelli put it: "To describe something as clever is not considered a compliment in the 
-Python culture." Python's philosophy rejects the Perl "there is more than one way to do it" approach to language design in favor 
-of "there should be one - and preferably only one - obvious way to do it".
+While offering choice in coding methodology, the Python philosophy rejects 
+exuberant syntax, such as in Perl, in favor of a sparser, less-cluttered 
+grammar. As Alex Martelli put it: "To describe something as clever is not 
+considered a compliment in the Python culture." Python's philosophy rejects 
+the Perl "there is more than one way to do it" approach to language design 
+in favor of "there should be one - and preferably only one - obvious way to 
+do it".
 
-Python's developers strive to avoid premature __word_3__, and moreover, reject patches to non-critical parts of CPython that 
-would offer a marginal increase in speed at the cost of clarity. When speed is important, a Python programmer can move time-critical 
-functions to extension modules written in languages such as C, or try using PyPy, a just-in-time compiler. __word_4__ is also available, 
-which translates a Python script into C and makes direct C-level API calls into the Python interpreter.
+Python's developers strive to avoid premature __word_3__, and moreover, 
+reject patches to non-critical parts of CPython that would offer a marginal 
+increase in speed at the cost of clarity. When speed is important, a Python 
+programmer can move time-critical functions to extension modules written in 
+languages such as C, or try using PyPy, a just-in-time compiler. __word_4__ 
+is also available, which translates a Python script into C and makes direct 
+C-level API calls into the Python interpreter.
 
-An important goal of Python's developers is making it fun to use. This is reflected in the origin of the name, which comes from 
-__word_5__, and in an occasionally playful approach to tutorials and reference materials, such as using examples that refer to 
-spam and eggs instead of the standard foo and bar.
+An important goal of Python's developers is making it fun to use. This is 
+reflected in the origin of the name, which comes from __word_5__, and in an 
+occasionally playful approach to tutorials and reference materials, such as 
+using examples that refer to spam and eggs instead of the standard foo and bar.
 
-A common neologism in the Python community is __word_6__, which can have a wide range of meanings related to program style. To say 
-that code is __word_6__ is to say that it uses Python idioms well, that it is natural or shows fluency in the language, that it 
-conforms with Python's minimalist philosophy and emphasis on readability. In contrast, code that is difficult to understand or 
-reads like a rough transcription from another programming language is called __word_7__.
+A common neologism in the Python community is __word_6__, which can have a 
+wide range of meanings related to program style. To say that code is 
+__word_6__ is to say that it uses Python idioms well, that it is natural or 
+shows fluency in the language, that it conforms with Python's minimalist 
+philosophy and emphasis on readability. In contrast, code that is difficult 
+to understand or reads like a rough transcription from another programming 
+language is called __word_7__.
 
-Users and admirers of Python, especially those considered knowledgeable or experienced, are often referred to as Pythonists, 
-Pythonistas, and Pythoneers.
+Users and admirers of Python, especially those considered knowledgeable or 
+experienced, are often referred to as Pythonists, Pythonistas, and Pythoneers.
 
 '''
 
@@ -120,7 +140,7 @@ def check_word(user_input,answer_list,position):
 #If OK, returns word input, otherwise, returns None.
 	user_compare = user_input.lower()
 	answer_index = position - 1
-	answer_compare = answer_list[answer_index]
+	answer_compare = answer_list[answer_index].lower()
 
 	if user_compare == answer_compare:
 		
@@ -142,35 +162,38 @@ def sub_words(selected_words, selected_string, tries_left):
 #	For every element of the selected answer list:
 #
 #		1. Joins current state of selected_list into a string, prints string,
-#		2. Asks user for next word (starting at 0)
+#		2. Asks user for next word (starting at 1)
 #		3. Calls check_word function
-#		4. If word matches, (i.e. return not None) replaces all instances of active word placeholders in selected_list
+#		4. If word matches, (i.e. return true) replaces all instances of active word placeholders in split_string
 #			with correct capitalisation
 #		5. Joins the substituted list back into a string
-#		6. Prints new substituted string, moves onto next word
+#		6. Moves onto next word
 #	If at any step the user input does not match the active element in the answer list:
 #		1. Subtracts 1 from number of tries left
 #		2. If resultant tries left > 0, goes back to the main loop
 #		3. Else, prints 'game over' string asks user for raw input y/n if they want to start again
 #				If y, calls difficulty function which resets the selected string, the number of tries left
 #				and then re-initialises the list
+#		
+#		Prints final string
 #	
 	
 	
-	result_list = []
+	
 	split_string = string_tosplit(selected_string)
 	for position, value in enumerate(selected_words,1):
-		answer_position = 'word ' + str(position)
-		
+		result_list = []
+		answer_position = 'word_' + str(position)
+
 		printed_string = "".join(split_string)
 		
 		print "---------------------------------------------------------------\n\
-The current paragraph reads:\n\n", printed_string
+The current string reads:\n\n", printed_string
 		
-		user_input = raw_input('\n\nType in your answer for ' + answer_position + ': ')
+		user_input = raw_input('\n\nPlease read the text above and then type in your answer for ' + answer_position + ': ')
 		
 		while check_word(user_input,selected_words,position) == None:
-			print check_word(user_input,selected_words,position)
+			
 			if tries_left > 1:
 				tries_left = tries_left -1
 				print '\nPlease try again, you have', tries_left, 'tries left.'
@@ -178,27 +201,29 @@ The current paragraph reads:\n\n", printed_string
 			else:
 				print 'That was incorrect and you have no tries left, goodbye!'
 				return None
-		if check_word(user_input,selected_words,position) != None:
-			'''
-			result_list = [n.replace(answer_position, ) for n in split_string]
+		if check_word(user_input, selected_words, position) == True:
+
 			for n in split_string:
 				
 				if n == answer_position:
-					n = n.replace(answer_position, selected_words[0])
+
+					n = n.replace(answer_position, selected_words[position-1])
+					
 					result_list.append(n)
 				else:
 					result_list.append(n)
-			print "".join(result_list)'''
-			print '\nThat is correct!'
-			
+					continue
+					
+					
+			split_string[:] = result_list
+
+			print '\nThat is correct!\n'
+	print "---------------------------------------------------------------\n\
+The complete string reads:\n\n", "".join(split_string), '\n\n Well done!'
 
 
-		
-
-
-
-tries_left = user_tries()
 user_difficulty = user_difficulty()
+tries_left = user_tries()
 
 selected_string = string_dict[user_difficulty]
 selected_words = words_dict[user_difficulty]
